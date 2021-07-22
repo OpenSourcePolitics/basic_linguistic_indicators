7391939a3a641eccd5252d9609bef5004a5a5411
QuentinLp <quentin.le-port@ensg.eu>
2021-07-22 17:02:48 +0200

    Update wordcloud creation and adding get_most_common_words

--- wordclouds_generation/wordcloud_creation.py
+++ wordclouds_generation/wordcloud_creation.py
@@ -1,23 +1,38 @@
 """
-This file is reponsible of wordclouds creation
+This file is responsible of wordclouds creation
 """
 import os
-import json
-import matplotlib.pyplot as plt
+from operator import itemgetter
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from utils.data_manipulation import load_data
 
 
-def create_wordcloud_from_frequency(file_path):
+def get_most_common_words(file_path, category=None, number_of_words=50):
+    """
+    This function will return the most common words
+    :param file_path:
+    :param category:
+    :param number_of_words: number of most common words
+    :return: list of most common words in the corpus
+    :rtype: list
+    """
+    data = load_data(file_path, category)
+    sorted_data = sorted(data.items(), key=itemgetter(1), reverse=True)
+    return sorted_data[:number_of_words]
+
+
+def create_wordcloud_from_frequency(file_path, category=None):
     """
     This function is used to create an image presenting a wordcloud based
     on a preprocessed file which will be grabbed from another docker image
     :param file_path: file to the json file storing the words and their associated frequency
     :type file_path: str
+    :param category: name of the category to study
     """
-    filename, file_extension = os.path.splitext(file_path)
+    filename, _ = os.path.splitext(file_path)
     filename = os.path.basename(os.path.normpath(filename))
-    with open(file_path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+    data = load_data(file_path, category)
     word_cloud = WordCloud(background_color="white", width=1200, height=600).generate_from_frequencies(data)
     plt.figure(figsize=(20, 10))
     plt.imshow(word_cloud, interpolation="bilinear")
