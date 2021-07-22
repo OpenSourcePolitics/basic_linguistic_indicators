37eebb70eb66141ec97fb096f3eb2a64e71b57f1
QuentinLp <quentin.le-port@ensg.eu>
2021-07-22 17:03:28 +0200

    Update imports and main execution

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
 import os
-from wordclouds_generation.wordcloud_creation import create_wordcloud_from_frequency
+from wordclouds_generation.wordcloud_creation import create_wordcloud_from_frequency, get_most_common_words
 MAIN_PATH = os.path.split(os.path.realpath(__file__))[0]
 
 
 if __name__ == '__main__':
     t1 = time.time()
-    create_wordcloud_from_frequency(os.path.join(MAIN_PATH, "dist/word_frequency_subset_raw_data.json"))
+    get_most_common_words(file_path=os.path.join(MAIN_PATH, "dist/word_frequency_test_preprocessed.json"))
+    create_wordcloud_from_frequency(os.path.join(MAIN_PATH, "dist/word_frequency_subset_raw_data_preprocessed.json"))
     print(time.time()-t1)
