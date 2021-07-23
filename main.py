adf8ee865ba34af5a66e3d4a71f09cd702beacf6
QuentinLp <quentin.le-port@ensg.eu>
2021-07-23 11:39:57 +0200

    Adding ldb creation in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import os
 from wordclouds_generation.wordcloud_creation import create_wordcloud_from_frequency, get_most_common_words
+from linguistic_database.ldb_creation import update_template_xlsx
 MAIN_PATH = os.path.split(os.path.realpath(__file__))[0]
 
 
@@ -8,4 +9,6 @@ if __name__ == '__main__':
     t1 = time.time()
     get_most_common_words(file_path=os.path.join(MAIN_PATH, "dist/word_frequency_test_preprocessed.json"))
     create_wordcloud_from_frequency(os.path.join(MAIN_PATH, "dist/word_frequency_subset_raw_data_preprocessed.json"))
+    update_template_xlsx(template_path=MAIN_PATH+"/criteria_template.xlsx",
+                         data_file_path=os.path.join(MAIN_PATH, "dist/word_frequency_test_preprocessed.json"))
     print(time.time()-t1)
