b184217d71c4124a54197a8a7a9e61297e42324e
QuentinLp <quentin.le-port@ensg.eu>
2021-08-11 14:31:54 +0200

    Change function entry along : from file to datastructures

--- linguistic_database/ldb_creation.py
+++ linguistic_database/ldb_creation.py
@@ -2,7 +2,6 @@
 This file will be responsible of the linguistic
 database creation. It will follow the information available in the template.
 """
-import os
 from openpyxl import load_workbook
 from linguistic_database.ldb_information_retrieval import prepare_data
 
@@ -24,28 +23,20 @@ def write_specific_cell(worksheet, list_values, column_letter):
         index += 1
 
 
-def update_template_xlsx(template_path, data_file_path, category=None):
+def update_template_xlsx(template_path: str, parsed_word_frequency_data: dict, category=None):
     """
     Updates the file criteria_template.xlsx:
         - appends the name of the data file
         - the linguistic indicators found in the data
         - names the worksheet with the category specified by the user
-    :param template_path: path to the template to update
-    :type template_path: str
-    :param data_file_path: path to the json storing the data
-    :type data_file_path: str
-    :param category: category used to subset the data
-    :type category: str
     """
-    filename = os.path.basename(os.path.normpath(os.path.splitext(data_file_path)[0]))
-    filename = filename[15:].replace("_", " ")
     workbook = load_workbook(filename=template_path)
     worksheet = workbook.active
     if category is not None:
         worksheet.title = category
     else:
         worksheet.title = "whole_data"
-    worksheet["C2"] = filename
-    list_values = prepare_data(data_file_path, category)
+    # worksheet["C2"] = filename
+    list_values = prepare_data(parsed_word_frequency_data)
     write_specific_cell(worksheet=worksheet, list_values=list_values, column_letter='C')
     workbook.save(filename=template_path)
--- linguistic_database/ldb_information_retrieval.py
+++ linguistic_database/ldb_information_retrieval.py
@@ -2,133 +2,102 @@
 This file stores functions that will analyze a
 json file and retrieve custom linguistic indicators
 """
-from utils.data_manipulation import load_data
 
 
-def get_total_word_count(file_path, category=None):
+def get_total_word_count(parsed_word_frequency_data: dict) -> int:
     """
-    Sums the values of the dictionary returned by load_data()
-    :param file_path: path to the json file storing the data
-    :type file_path: str
-    :param category: category specified by the user used to subset the data
-    :type category: str
-    :return: sum of dict values
-    :rtype: int
+    Sums the values of the dictionary returned by parse_data()
     """
-    data = load_data(file_path, category)
-    return sum(data.values())
+    return sum(parsed_word_frequency_data.values())
 
 
-def specific_vocabulary_retrieval(data_file_path, words_list, category=None):
+def specific_vocabulary_retrieval(parsed_word_frequency_data: dict, linguistic_indicators_list: list) -> list:
     """
-    Retrieve the values of specific words from the json data.
+    Retrieve a list of frequencies of the specified words from the word frequency data.
     The parameter category is used to subset the corpus.
-    :param data_file_path: path to the json object storing the data
-    :type data_file_path: str
-    :param words_list: list of linguistic indicators
-    :type words_list: list
-    :param category: category specified by the user used to subset the data
-    :type category: str
-    :return: list of values corresponding to the input words
-    :rtype: list
     """
     meaningful_words = []
-    data_file_path = load_data(data_file_path, category)
-    for word in words_list:
-        if word not in list(data_file_path.keys()):
+    for word in linguistic_indicators_list:
+        if word not in list(parsed_word_frequency_data.keys()):
             meaningful_words.append(0)
         else:
-            meaningful_words.append(data_file_path[word])
+            meaningful_words.append(parsed_word_frequency_data[word])
     return meaningful_words
 
 
-def multi_criteria_extraction(data_file_path, word_list, category=None):
+def multi_criteria_extraction(parsed_word_frequency_data: dict,
+                              linguistic_indicators_list: list) -> int:
     """
     Aggregates the frequencies of the words passed as an input. It is used to retrieve a global
     metric for several verb forms for instance.
     The parameter category is used to subset the corpus.
-    :param data_file_path: path to the json object storing the data
-    :type data_file_path: str
-    :param word_list: list of linguistic indicators
-    :type word_list: list
-    :param category: category specified by the user used to subset the data
-    :type category: str
-    :return: sum of the values
-    :rtype: int
     """
-    all_forms = specific_vocabulary_retrieval(data_file_path, word_list, category)
+    all_forms = specific_vocabulary_retrieval(parsed_word_frequency_data, linguistic_indicators_list)
     return sum(all_forms)
 
 
-def get_conditional_markers(data_file_path, category=None):
+def get_conditional_markers(parsed_word_frequency_data: dict) -> int:
     """
     Retrieves the sum of the frequencies associated with verbs that shows marks of
     conditional (french conjugation)
     The parameter category is used to subset the corpus.
-    :param data_file_path: path to the json object storing the data
-    :type data_file_path: str
-    :param category: category specified by the user used to subset the data
-    :type category: str
-    :return: sum of the values of the initial json file
-    :rtype: int
     """
     conditional_markers = []
-    data = load_data(data_file_path, category)
-    for word in data.keys():
+    for word in parsed_word_frequency_data.keys():
         if word.endswith("rait") \
                 or word.endswith("raient") \
                 or word.endswith("riez") \
                 or word.endswith("rions"):
-            conditional_markers.append(data[word])
+            conditional_markers.append(parsed_word_frequency_data[word])
     return sum(conditional_markers)
 
 
-def prepare_data(data_file_path, category):
+def prepare_data(parsed_word_frequency_data: dict):
     """
     Functions used to retrieve the information of a custom list of
     french linguistic indicators. Returns a list of value corresponding to their
     frequency or aggregated frequency according to the function used to retrieve the information.
-    :param data_file_path: path to the json object storing the data
-    :type data_file_path: str
-    :param category: category specified by the user used to subset the data
-    :type category: str
     :return: list of frequency
     :rtype: list
     """
-    point_of_view = specific_vocabulary_retrieval(data_file_path,
-                                                  words_list=[
+    point_of_view = specific_vocabulary_retrieval(parsed_word_frequency_data,
+                                                  linguistic_indicators_list=[
                                                       "je", "vous", "nous"
-                                                  ],
-                                                  category=category)
-    deliberative_words = specific_vocabulary_retrieval(data_file_path,
-                                                       words_list=["mais", "car", "donc",
-                                                                   "malgré", "pourtant",
-                                                                   "cependant"],
-                                                       category=category)
-    adverbs = specific_vocabulary_retrieval(data_file_path,
-                                            words_list=["jamais", "toujours",
-                                                        "systématiquement", "!",
-                                                        "éventuellement"
-                                                        ],
-                                            category=category)
-    values = [get_total_word_count(data_file_path, category),
-              multi_criteria_extraction(data_file_path, word_list=["http", "https"], category=category),
-              multi_criteria_extraction(data_file_path, word_list=["exemple", "exemples"], category=category),
-              multi_criteria_extraction(data_file_path, word_list=["faut", "falloir", "faudra"], category=category),
-              multi_criteria_extraction(data_file_path, word_list=["pouvoir", "peut", "peuvent",
-                                                                   "pouvons", "pourra", "pourront",
-                                                                   "pourrons"], category=category),
-              multi_criteria_extraction(data_file_path, word_list=["devoir", "doit", "doivent",
-                                                                   "devra", "devrait", "devraient",
-                                                                   "devront", "devrons"], category=category),
-              get_conditional_markers(data_file_path, category=category)]
+                                                  ])
+    deliberative_words = specific_vocabulary_retrieval(parsed_word_frequency_data,
+                                                       linguistic_indicators_list=["mais", "car", "donc",
+                                                                                   "malgré", "pourtant",
+                                                                                   "cependant"])
+    adverbs = specific_vocabulary_retrieval(parsed_word_frequency_data,
+                                            linguistic_indicators_list=["jamais", "toujours",
+                                                                        "systématiquement", "!",
+                                                                        "éventuellement"
+                                                                        ])
+    values = [get_total_word_count(parsed_word_frequency_data),
+              multi_criteria_extraction(parsed_word_frequency_data,
+                                        linguistic_indicators_list=["http", "https"]),
+              multi_criteria_extraction(parsed_word_frequency_data,
+                                        linguistic_indicators_list=["exemple", "exemples"]),
+              multi_criteria_extraction(parsed_word_frequency_data,
+                                        linguistic_indicators_list=["faut", "falloir", "faudra"]),
+              multi_criteria_extraction(parsed_word_frequency_data,
+                                        linguistic_indicators_list=["pouvoir", "peut", "peuvent",
+                                                                    "pouvons", "pourra", "pourront",
+                                                                    "pourrons"]),
+              multi_criteria_extraction(parsed_word_frequency_data,
+                                        linguistic_indicators_list=["devoir", "doit", "doivent",
+                                                                    "devra", "devrait", "devraient",
+                                                                    "devront", "devrons"]),
+              get_conditional_markers(parsed_word_frequency_data)]
     values += point_of_view
     values += deliberative_words
     values += adverbs
-    values.append(multi_criteria_extraction(data_file_path, word_list=["évident", "évidemment"], category=category))
-    values.append(multi_criteria_extraction(data_file_path, word_list=["indéniable", "indéniablement"],
-                                            category=category))
-    values.append(multi_criteria_extraction(data_file_path,
-                                            word_list=["nécessaire", "nécessaires", "nécessite", "nécessitent",
-                                                       "nécessairement", "besoin", "besoins"], category=category))
+    values.append(multi_criteria_extraction(parsed_word_frequency_data,
+                                            linguistic_indicators_list=["évident", "évidemment"]))
+    values.append(multi_criteria_extraction(parsed_word_frequency_data,
+                                            linguistic_indicators_list=["indéniable", "indéniablement"]))
+    values.append(multi_criteria_extraction(parsed_word_frequency_data,
+                                            linguistic_indicators_list=["nécessaire", "nécessaires", "nécessite",
+                                                                        "nécessitent",
+                                                                        "nécessairement", "besoin", "besoins"]))
     return values
